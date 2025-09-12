44ab2c7d9aef01da165ab447d3de2ff8de6d907d
simone papicchio <simopapi@hotmail.com>
2025-09-12 19:08:07 +0200

    fix: error prompt.append() return None

--- src/think2sql/data_processor/messages_builder.py
+++ src/think2sql/data_processor/messages_builder.py
@@ -1,8 +1,13 @@
 from jinja2 import Environment, FileSystemLoader, StrictUndefined
 
 
-def build_messages(row, system_prompt_name, user_prompt_name, prompt_folder: str,
-                   assistant_response_col_name: str = None, ):
+def build_messages(
+        row,
+        system_prompt_name,
+        user_prompt_name,
+        prompt_folder: str,
+        assistant_response_col_name: str | None = None,
+):
     system_prompt = _render_prompt(system_prompt_name, prompt_folder, **row)
     user_prompt = _render_prompt(user_prompt_name, prompt_folder, **row)
 
@@ -11,7 +16,7 @@ def build_messages(row, system_prompt_name, user_prompt_name, prompt_folder: str
         {"role": "user", "content": user_prompt},
     ]
     if assistant_response_col_name:
-        prompt = prompt.append(
+        prompt.append(
             {"role": "assistant", "content": row[assistant_response_col_name]}
         )
 
@@ -25,7 +30,7 @@ def _render_prompt(template_name: str, prompt_folder: str, **data) -> str:
         undefined=StrictUndefined,  # fail fast on missing vars
         trim_blocks=True,  # nicer whitespace
         lstrip_blocks=True,  # ^
-        keep_trailing_newline=True
+        keep_trailing_newline=True,
     )
 
     tmpl = env.get_template(template_name)
@@ -35,13 +40,13 @@ def _render_prompt(template_name: str, prompt_folder: str, **data) -> str:
 if __name__ == "__main__":
     a = build_messages(
         {
-            'question': 'What is the capital of France?',
-            'evidence': 'France is a country in Europe. Its capital is Paris.',
-            'schema': 'Question: <question>\nAnswer: <answer>'
+            "question": "What is the capital of France?",
+            "evidence": "France is a country in Europe. Its capital is Paris.",
+            "schema": "Question: <question>\nAnswer: <answer>",
         },
-        'base_think_system_prompt.jinja',
-        'base_think_user_prompt.jinja',
-        'prompts'
+        "base_think_system_prompt.jinja",
+        "base_think_user_prompt.jinja",
+        "prompts",
     )
 
     print(a)
