fa81301fbb4cbd565f651217579b417a37bbdabb
simone papicchio <simopapi@hotmail.com>
2025-09-12 19:13:14 +0200

    feat: add for SQL rewards check possible CRUD operations over the DB. CRUD operations have 0 rewards

--- src/think2sql/grpo/rewards.py
+++ src/think2sql/grpo/rewards.py
@@ -1,63 +1,95 @@
 import re
+from statistics import mean
 from typing import Callable
 
-import NL2SQLEvaluator.main_run_evaluation as sql_evaluator
-from NL2SQLEvaluator.orchestrator_state import AvailableMetrics
+from NL2SQLEvaluator.metric_executor.evaluator_orchestrator import OrchestratorInput
+from NL2SQLEvaluator.metric_executor.evaluator_orchestrator import (
+    evaluator_orchestrator,
+)
+from NL2SQLEvaluator.orchestrator_state import AvailableMetrics, AvailableDialect
 
 from think2sql.configs import GRPOScriptArguments
 from think2sql.logger import get_logger
+from think2sql.utils.data import utils_get_engine
 from think2sql.utils.sql import get_sql_from_generation
 
 
-def get_execution_acc_reward(relative_db_base_path):
-    def ex_reward(completions: list[dict], target_sql: list[str], db_id: list[str], *args, **kwargs):
-        model_prediction = [completion[0]["content"] for completion in completions]
-        predicted_sqls = [get_sql_from_generation(pred) for pred in model_prediction]
-        data = [
-            sql_evaluator.DatasetRow(target_query=sql, predicted_query=pred, db_id=db_id_)
-            for sql, pred, db_id_ in zip(target_sql, predicted_sqls, db_id)
-        ]
-        script_args_evaluator = sql_evaluator.ScriptArgs(
-            metrics=[AvailableMetrics.EXECUTION_ACCURACY],
-            relative_db_base_path=relative_db_base_path,
-            dataset=data,
-            column_name_target='target_query',
-            column_name_predicted='predicted_query',
-            column_name_db_id='db_id',
-        )
+def _check_crud_sql_(query):
+    if (
+            "INSERT" in str(query).upper()
+            or "UPDATE" in str(query).upper()
+            or "DELETE" in str(query).upper()
+            or "CREATE" in str(query).upper()
+            or "DROP" in str(query).upper()
+            or "ALTER" in str(query).upper()
+    ):
+        return "WRONG_QUERY"
+    return query
 
-        _, df_completed_tasks = sql_evaluator.run_evaluation(script_args_evaluator)
-        return df_completed_tasks['execution_accuracy'].tolist()
+
+def get_execution_acc_reward(relative_db_base_path):
+    def ex_reward(
+            completions: list[dict],
+            target_sql: list[str],
+            db_id: list[str],
+            *args,
+            **kwargs,
+    ):
+        model_prediction = [completion[0]["content"] for completion in completions]
+        predicted_sqls = [
+            _check_crud_sql_(get_sql_from_generation(pred)) for pred in model_prediction
+        ]
+
+        orchestrator_input = OrchestratorInput(
+            target_queries=target_sql,
+            predicted_queries=predicted_sqls,
+            executor=[
+                utils_get_engine(relative_db_base_path, AvailableDialect("sqlite"), id_)
+                for id_ in db_id
+            ],
+            metrics_to_calculate=[
+                AvailableMetrics.EXECUTION_ACCURACY,
+            ],
+        )
+        results = evaluator_orchestrator.invoke(orchestrator_input)
+        results = [
+            result[AvailableMetrics.EXECUTION_ACCURACY.value] for result in results
+        ]
+        return results
 
     return ex_reward
 
 
 def get_qatch_reward(relative_db_base_path):
-    def qatch_reward(completions: list[dict], target_sql: list[str], db_id: list[str], *args, **kwargs):
+    def qatch_reward(
+            completions: list[dict],
+            target_sql: list[str],
+            db_id: list[str],
+            *args,
+            **kwargs,
+    ):
         model_prediction = [completion[0]["content"] for completion in completions]
-        predicted_sqls = [get_sql_from_generation(pred) for pred in model_prediction]
-        data = [
-            sql_evaluator.DatasetRow(target_query=sql, predicted_query=pred, db_id=db_id_)
-            for sql, pred, db_id_ in zip(target_sql, predicted_sqls, db_id)
+        predicted_sqls = [
+            _check_crud_sql_(get_sql_from_generation(pred)) for pred in model_prediction
         ]
-        script_args_evaluator = sql_evaluator.ScriptArgs(
-            metrics=[AvailableMetrics.CELL_PRECISION,
-                     AvailableMetrics.CELL_RECALL,
-                     AvailableMetrics.TUPLE_CARDINALITY],
-            relative_db_base_path=relative_db_base_path,
-            dataset=data,
-            column_name_target='target_query',
-            column_name_predicted='predicted_query',
-            column_name_db_id='db_id',
+        orchestrator_input = OrchestratorInput(
+            target_queries=target_sql,
+            predicted_queries=predicted_sqls,
+            executor=[
+                utils_get_engine(relative_db_base_path, AvailableDialect("sqlite"), id_)
+                for id_ in db_id
+            ],
+            metrics_to_calculate=[
+                AvailableMetrics.CELL_PRECISION,
+                AvailableMetrics.CELL_RECALL,
+                AvailableMetrics.TUPLE_CARDINALITY,
+            ],
         )
-
-        _, df_completed_tasks = sql_evaluator.run_evaluation(script_args_evaluator)
-        # make a third column with the mean of the three metrics above
-        df_completed_tasks['qatch_metric'] = df_completed_tasks[
-            ['cell_precision', 'cell_recall', 'tuple_cardinality']
-        ].mean(axis=1)
-
-        return df_completed_tasks['qatch_metric'].tolist()
+        results = evaluator_orchestrator.invoke(orchestrator_input)
+        results = [
+            mean(list(result.values())) for result in results
+        ]
+        return results
 
     return qatch_reward
 
@@ -88,7 +120,10 @@ def format_reward(completions, **kwargs):
     """Reward function that checks if the reasoning process is enclosed within <think> and </think> tags, while the final answer is enclosed within <answer> and </answer> tags."""
     pattern = r"^<think>\n.*?\n</think>\n<answer>\n.*?\n</answer>$"
     completion_contents = [completion[0]["content"] for completion in completions]
-    matches = [re.match(pattern, content, re.DOTALL | re.MULTILINE) for content in completion_contents]
+    matches = [
+        re.match(pattern, content, re.DOTALL | re.MULTILINE)
+        for content in completion_contents
+    ]
     return [1.0 if match else 0.0 for match in matches]
 
 
@@ -103,5 +138,5 @@ def get_reward_funcs(script_args: GRPOScriptArguments) -> list[Callable]:
     }
 
     reward_funcs = [REWARD_FUNCS_REGISTRY[func] for func in script_args.reward_funcs]
-    logger.info(f'loaded reward functions: {script_args.reward_funcs}')
+    logger.info(f"loaded reward functions: {script_args.reward_funcs}")
     return reward_funcs
